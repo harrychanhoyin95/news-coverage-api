954354f18c60fc83834ddff87ecdf9fa357f95f8
harrychanhoyin95 <harrychanhoyin95@gmail.com>
2020-07-07 00:56:02 +0800

    fix: dotenv

--- src/config/index.js
+++ src/config/index.js
@@ -2,10 +2,12 @@ import dotenv from 'dotenv';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const envFound = dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  const envFound = dotenv.config();
 
-if (envFound.error) {
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  if (envFound.error) {
+    throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  }
 }
 
 export default {
